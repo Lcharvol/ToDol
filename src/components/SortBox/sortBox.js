0c1b563fdeae3fb307fc0b66fbd10128465bfb40
Lucas CHARVOLIN <lcharvol@e3r2p18.42.fr>
2017-07-17 18:19:01 +0200

    update

--- src/components/SortBox/sortBox.js
+++ src/components/SortBox/sortBox.js
@@ -9,25 +9,35 @@ class SortBox extends Component {
   }
 
   displaySortTab = () => {
-    console.log("coucou");
+    if (this.state.wrap === true) {
+    this.setState({wrap: false});}
+    else {
+      this.setState({wrap: true});
+    }
   }
 
   render() {
     const { wrap } = this.state;
 
+    const divStyle = {
+      width: wrap ? '130px' : '300px',
+    }
+
     return (
       <div className="sortBoxContainer">
         <div className="sortBoxContainerInner">
-          <div className="sortBox">
+          <div className="sortBox" style={divStyle}>
             <div className="sortBoxRightSide">
               <p className="sortBy">Sort by</p>
             </div>
             <div className="chevContainer" onClick={this.displaySortTab}>
               <i className="fa fa-chevron-right chevfilter" aria-hidden="true" onClick={this.displaySortTab}/>
             </div>
+            {!wrap &&
+              <div className="sortTab">
+              </div>
+            }
           </div>
-          {!wrap && <div className="sortTab">
-          </div>}
         </div>
       </div>
     );
