a2423b7ef5eb9574c4dbb70db3dafa4748706282
Lucas CHARVOLIN <lcharvol@e3r4p7.42.fr>
2017-07-18 16:37:31 +0200

    upgrade page404

--- src/containers/page404/page404.js
+++ src/containers/page404/page404.js
@@ -2,10 +2,16 @@ import React from 'react';
 
 import './page404.css';
 
+import Header from '../../components/Header';
+
 const page404 = () => (
   <div className="main404">
-    <h1>ERROR 404</h1>
-    <h1>PAGE NOT FOUND</h1>
+    <div className="containers404">
+      <i className="fa fa-smile-o smiley" aria-hidden="true"></i>
+      <h1>ERROR 404</h1>
+      <h1>Oooop, something</h1>
+      <h1>went wrong</h1>
+  </div>
   </div>
 )
 
